fb0bd93590d39f6d371f75f93696a0eac163ffcf
Kourieh Edouard <malkekourie@hotmail.com>
2022-03-24 14:07:53 +0100

    add the navigation

---
+++ frontend/src/components/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React from "react";
+
+class App extends React.Component{
+	render(){
+		return(
+			<div className="App">
+				<h1>Home</h1>
+				<div className="slider">
+					<div className="slides">
+						<div className="slide"><img src={require('../pictures/image1.jpg')} alt="img1" width="500px" height="200px"/></div>
+						<div className="slide"><img src={require('../pictures/image2.jpg')} alt="img2" width="500px"  height="200px" /></div>
+						<div className="slide"><img src={require('../pictures/image3.jpg')} alt="img3" width="500px" height="200px" /></div>
+					</div>
+				</div>
+		    </div>
+		)
+	}
+}
+
+export default App;
+
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Signup extends React.Component{
+	render(){
+		return(
+			<h1>Sign Up</h1>
+		)
+	}
+}
+
+export default Signup;
