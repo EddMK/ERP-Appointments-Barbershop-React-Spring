5ef1cadd061737a4445e5e104c5e1e256dde5183
Kourieh Edouard <malkekourie@hotmail.com>
2022-03-26 22:21:37 +0100

    input signup

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,19 +1,122 @@
 import React, { Component } from "react";
 import TextField from '@mui/material/TextField';
+import './Signup.css';
+import InputAdornment from '@mui/material/InputAdornment';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
+import PasswordIcon from '@mui/icons-material/Password';
+import Button from '@mui/material/Button';
+
 
 
 class Signup extends React.Component{
 	render(){
 		return(
-			<div>
+			<div className="signup">
 				<h1>Sign Up</h1>
-				<TextField id="outlined-basic" label="Last Name" variant="outlined" />
-				<TextField id="outlined-basic" label="First Name" variant="outlined" />
-				<TextField id="outlined-basic" label="E-mail" variant="outlined" />
-				<TextField id="outlined-basic" label="Phone Number" variant="outlined" />
-				<TextField id="outlined-basic" label="Picture" variant="outlined" />
-				<TextField id="outlined-basic" label="Password" variant="outlined" />
-				<TextField id="outlined-basic" label="Confirm Password" variant="outlined" />
+				<div className="textfield">
+					<TextField 
+						fullWidth 
+						id="outlined-basic" 
+						label="Last Name" 
+						InputProps={{
+							startAdornment: (
+								<InputAdornment position="start">
+									<AccountCircle />
+								</InputAdornment>
+							),
+						}} 
+						variant="outlined" />
+				</div>
+				<div className="textfield">
+					<TextField 
+						fullWidth 
+						id="outlined-basic" 
+						label="First Name" 
+						InputProps={{
+							startAdornment: (
+								<InputAdornment position="start">
+									<AccountCircle />
+								</InputAdornment>
+							),
+						}} 
+						variant="outlined" />
+				</div>
+				<div className="textfield">
+					<TextField 
+						fullWidth 
+						id="outlined-basic" 
+						label="E-mail" 
+						InputProps={{
+							startAdornment: (
+								<InputAdornment position="start">
+									<AlternateEmailIcon />
+								</InputAdornment>
+							),
+						}} 
+						variant="outlined" />
+				</div>
+				<div className="textfield">
+					<TextField 
+						fullWidth 
+						id="outlined-basic" 
+						label="Phone Number" 
+						InputProps={{
+							startAdornment: (
+								<InputAdornment position="start">
+									<LocalPhoneIcon />
+								</InputAdornment>
+							),
+						}} 
+						variant="outlined" />
+				</div>
+				<div className="textfield">
+					<TextField 
+						fullWidth 
+						id="outlined-basic" 
+						label="Picture" 
+						InputProps={{
+							startAdornment: (
+								<InputAdornment position="start">
+									<InsertPhotoIcon />
+								</InputAdornment>
+							),
+						}} 
+						variant="outlined" />
+				</div>
+				<div className="textfield">
+					<TextField 
+						fullWidth 
+						id="outlined-basic" 
+						label="Password" 
+						InputProps={{
+							startAdornment: (
+								<InputAdornment position="start">
+									<PasswordIcon />
+								</InputAdornment>
+							),
+						}} 
+						variant="outlined" />
+				</div>
+				<div className="textfield">
+					<TextField 
+						fullWidth 
+						id="outlined-basic" 
+						label="Confirm Password" 
+						InputProps={{
+							startAdornment: (
+								<InputAdornment position="start">
+									<PasswordIcon />
+								</InputAdornment>
+							),
+						}} 
+						variant="outlined" />
+				</div>
+				<div className="Bouton">
+					<Button variant="contained" onClick={() => { this.handleClick() ;}}>Confirm</Button>
+				</div>
 			</div>
 		)
 	}
