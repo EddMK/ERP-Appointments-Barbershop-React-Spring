b9ea9773ff3e02cb65eb5a13a594dee1c489d490
Kourieh Edouard <malkekourie@hotmail.com>
2022-03-26 10:17:46 +0100

    textfield sign up

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import TextField from '@mui/material/TextField';
+
 
 class Signup extends React.Component{
 	render(){
 		return(
-			<h1>Sign Up</h1>
+			<div>
+				<h1>Sign Up</h1>
+				<TextField id="outlined-basic" label="Last Name" variant="outlined" />
+				<TextField id="outlined-basic" label="First Name" variant="outlined" />
+				<TextField id="outlined-basic" label="E-mail" variant="outlined" />
+				<TextField id="outlined-basic" label="Phone Number" variant="outlined" />
+				<TextField id="outlined-basic" label="Picture" variant="outlined" />
+				<TextField id="outlined-basic" label="Password" variant="outlined" />
+				<TextField id="outlined-basic" label="Confirm Password" variant="outlined" />
+			</div>
 		)
 	}
 }
