2ef264eedeba24ebd9d2dd42fd7eab62542fb1f7
Kourieh Edouard <malkekourie@hotmail.com>
2022-03-27 19:23:20 +0200

    login inputs

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import './Login.css'
 
 class Login extends React.Component{
 	render(){
 		return(
-			<h1>Log In</h1>
+			<div className="login">
+				<h1>Log In</h1>
+				<div className="textfield">
+					<TextField 
+						fullWidth 
+						id="outlined-basic" 
+						label="Last Name" 
+						variant="outlined" />
+				</div>
+				<div className="textfield">
+					<TextField 
+						type="password"
+						fullWidth 
+						id="outlined-basic" 
+						label="Password" 
+						variant="outlined" />
+				</div>
+				<div className="bouton">
+					<Button variant="contained" onClick={() => { this.handleClick() ;}}>Log In</Button>
+				</div>
+			</div>
 		)
 	}
 }
