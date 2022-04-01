1e5dfde65c875d25a7c568a71e1f58814098dce9
Kourieh Edouard <malkekourie@hotmail.com>
2022-04-01 23:17:15 +0200

    bug when add appointement

--- frontend/src/components/Schedule.js
+++ frontend/src/components/Schedule.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import './Schedule.css';
 import TextField from '@mui/material/TextField';
+import Input from '@mui/material/Input';
 import Button from '@mui/material/Button';
 import Autocomplete from '@mui/material/Autocomplete';
 import Paper from '@mui/material/Paper';
@@ -17,7 +18,7 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import TimePicker from '@mui/lab/TimePicker';
 
 const top100Films = ['coupe','barbe'];
-const currentDate = '2022-04-01';
+//const currentDate = '2022-04-01';
 const Appointment = ({
     children, style, isShaded, ...restProps
   }) => (
@@ -37,12 +38,17 @@ const Appointment = ({
 class Schedule extends React.Component{
 
     constructor(props){
-      super();
+      super(props);
       this.state={
         schedulerData : [
-          { startDate: '2022-04-01T10:20', endDate: '2022-04-01T11:00', title: 'taken' },
-          { startDate: '2022-04-01T12:00', endDate: '2022-04-01T13:30', title: 'taken' },
-        ]
+          { startDate: '2022-04-09T10:20', endDate: '2022-04-09T11:00', title: 'taken' },
+          { startDate: '2022-04-09T12:00', endDate: '2022-04-09T13:30', title: 'taken' },
+        ],
+        date : this.props.date,
+        hairdresser : this.props.hairdresser,
+        service : null,
+        startAppointement : null,
+        endAppointement : null
       }
       this.handleTimePicker = this.handleTimePicker.bind(this);
       this.handleCancel = this.handleCancel.bind(this);
@@ -50,15 +56,30 @@ class Schedule extends React.Component{
     }
 
     handleTimePicker(value){
+      this.setState({
+        startAppointement : value
+      });
       //schedulerData.concat({startDate:"2022-04-01T15:20",endDate:"2022-04-01T16:20", title:"token"});
-      //alert(value);
+      console.log(value.hours(), value.minutes());
     }
 
     handleConfirm(){
       //schedulerData(...{startDate:"2022-04-01T15:20",endDate:"2022-04-01T16:20", title:"token"});
+      //if(this.state.startAppointement != null &&  this.state.service != null){
+      if(this.state.startAppointement != null){
+        this.state.startAppointement.set('year', this.state.date.year());
+        this.state.startAppointement.set('month', this.state.date.month());
+        this.state.startAppointement.set('date', this.state.date.date());
+        console.log(this.state.startAppointement);
+        console.log(this.state.startAppointement.add(30, 'minutes').calendar());
+        this.setState({
+          schedulerData : [...this.state.schedulerData,{startDate: this.state.startAppointement ,endDate: this.state.startAppointement.add(30, 'minutes'), title:"token"}]
+        });
+      }
+      /*
       this.setState({
         schedulerData : [...this.state.schedulerData,{startDate:"2022-04-01T15:20",endDate:"2022-04-01T16:20", title:"token"}]
-      });
+      });*/
     }
 
     handleCancel(){
@@ -70,14 +91,13 @@ class Schedule extends React.Component{
             <div className="Schedule">
               <h1>Schedule</h1>
               <div className="form">
-                  <p>Choose service and time</p>
+                  {this.state.hairdresser}
                   <Autocomplete
                     id="combo-box-demo"
                     options={top100Films}
                     style={{ width: 300 }}
                     renderInput={(params) => <TextField {...params} label="Choose the service" variant="outlined" />}
                   />
-                  <p>Time Picker</p>
                   <LocalizationProvider dateAdapter={DateAdapter}>
                     <TimePicker
                       label="Basic example"
@@ -86,7 +106,7 @@ class Schedule extends React.Component{
                         }}
                       renderInput={(params) => <TextField {...params} />}
                     />
-                </LocalizationProvider>
+                  </LocalizationProvider>
                 </div>
                 <div className="hour">
                 <Box
@@ -96,7 +116,7 @@ class Schedule extends React.Component{
                   }}
                 >
                   <Scheduler data={this.state.schedulerData} >
-                      <ViewState currentDate={currentDate} />
+                      <ViewState currentDate={this.state.date} />
                       <DayView startDayHour={10} endDayHour={20} />
                       <Appointments appointmentComponent={Appointment} />
                       <CurrentTimeIndicator
