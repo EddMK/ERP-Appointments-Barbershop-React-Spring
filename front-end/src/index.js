a14c2245e6fe0670d5b8fee993cc8a90a3224cf2
Kourieh Edouard <malkekourie@hotmail.com>
2022-04-05 18:05:00 +0200

    change repository

---
+++ front-end/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Signup from './components/Signup.js';
+import App from './components/App';
+import Login from './components/Login';
+import Agenda from './components/Agenda';
+import Menu from './components/Menu';
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+          	<Menu />
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/agenda" element={<Agenda />} />
+			</Routes>
+		</BrowserRouter>
+  </React.StrictMode>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
