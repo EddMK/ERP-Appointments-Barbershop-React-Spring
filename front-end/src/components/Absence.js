bb03e2c29a193950dd10e87ca27cb9999dff6dda
Kourieh Edouard <malkekourie@hotmail.com>
2022-08-25 16:39:50 +0200

    localisation time

--- front-end/src/components/Absence.js
+++ front-end/src/components/Absence.js
@@ -3,6 +3,8 @@ import {Dialog,DialogTitle, DialogContent, TextField} from '@mui/material/';
 import {LocalizationProvider, DatePicker} from '@mui/x-date-pickers/';
 import DateAdapter from '@mui/lab/AdapterMoment';
 import moment from "moment";
+//import frLocale from 'date-fns/locale/fr';
+import 'moment/locale/fr'
 
 
 export default class Absence extends React.Component{
@@ -11,8 +13,9 @@ export default class Absence extends React.Component{
         super(props)
         console.log(this.props.date);
         this.state = {
-            date : this.props.date,
-            value : moment(this.props.date)
+            date : moment(this.props.date),
+            min : moment(this.props.date).startOf('week'),
+            max : moment(this.props.date).endOf('week'),
         };
     }
 
@@ -21,16 +24,18 @@ export default class Absence extends React.Component{
             <Dialog onClose={this.props.close} open={true}>
                     <DialogTitle sx={{ textAlign : "center"}} >Absence</DialogTitle>
                     <DialogContent>
-                    <LocalizationProvider dateAdapter={DateAdapter}>
-                        <DatePicker
-                            label="Choose the day"
-                            value={this.state.value}
-                            onChange={(newValue) => {
-                                this.setState({value : newValue})
-                            }}
-                            renderInput={(params) => <TextField {...params} />}
-                        />
-                    </LocalizationProvider>
+                        <LocalizationProvider dateAdapter={DateAdapter} >
+                            <DatePicker
+                                label="Choose the day"
+                                value={this.state.value}
+                                minDate={this.state.min}
+                                maxDate={this.state.max}
+                                onChange={(newValue) => {
+                                    this.setState({value : newValue})
+                                }}
+                                renderInput={(params) => <TextField {...params} />}
+                            />
+                        </LocalizationProvider>
                     </DialogContent>
             </Dialog>
         )
